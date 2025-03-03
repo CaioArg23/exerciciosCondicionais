8bed86754efcad688b9c99a2bbba91917b6c7e35
CaioArg23 <caio21lf@gmail.com>
2025-03-03 12:33:52 -0300

    terceiro commit

--- 15.py
+++ 15.py
@@ -1,3 +1,18 @@
-n1 = 100
-n2 = n1 / 100
-print (100%2)
+dia = int(input('digite o dia: '))
+mes = int(input('digite o número do mês: '))
+ano = int(input('digite o ano: '))
+
+if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+    if dia > 31:
+        print('digite um dia menor ou igual a 31')
+
+elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+    if dia > 30:
+        print('digite um dia menor ou igual a 30')
+
+elif mes == 2:
+    if dia > 28:
+        print('digite um dia menor ou igual a 28')
+
+
+print(f'{dia}/{mes}/{ano}')
